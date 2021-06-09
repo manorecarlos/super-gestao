771675eae12c323b2f1602dacf0f16ab616b8552
manorecarlos <renancarlos3010@gmail.com>
2021-06-09 20:31:44 -0300

    rotas e controllers iniciais

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Rotas da aplicação */
+
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
