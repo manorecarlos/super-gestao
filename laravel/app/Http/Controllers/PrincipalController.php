9c740171c20c61a8cd62e66f80fe994d4998904d
manorecarlos <renancarlos3010@gmail.com>
2021-06-11 18:30:40 -0300

    inicio blades

--- laravel/app/Http/Controllers/PrincipalController.php
+++ laravel/app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        echo "Hello World!";
+        return view('site.principal');
     }
 }
